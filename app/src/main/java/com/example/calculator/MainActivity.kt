e00dcf5809642e712b195d933c9aa26d57cddc90
Raef Nazir <raefnazir@gmail.com>
2023-12-14 18:01:38 +0400

    implementing BODMAS

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 import java.lang.Exception
 import java.util.Stack
 
@@ -78,7 +79,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun onEqualsButtonClick(view: View){
         try{
-            val result = evaluateExpression(currentInput.toString())
+            val result = calculator.evaluateExpression(currentInput.toString())
+            /*val result = evaluateExpression(currentInput.toString())*/
             currentInput.clear()
             currentInput.append(result)
             updateResultTextView()
@@ -126,7 +128,7 @@ class MainActivity : AppCompatActivity() {
         resultTextView.text = currentInput.toString()
     }
 
-    private fun evaluateExpression(expression: String):String{
+    private fun evaluateExpression(expression: String):Double{
         //val elements = expression.split(Regex("([^0-9.])"))
         val numbers = expression.split(Regex("([^0-9.])")).filter { it.isNotBlank() && it!="." }.map { it.toDoubleOrNull() ?: throw IllegalArgumentException("Invalid number: $it")}
         val operators = expression.split(Regex("([0-9.])")).filter { it.isNotBlank() }
@@ -140,7 +142,7 @@ class MainActivity : AppCompatActivity() {
                 "%" -> result %= numbers[i]
             }
         }
-        return result.toString()
+        return result
         currentInput.clear()
         currentInput.append(result.toString())
     }
@@ -148,10 +150,10 @@ class MainActivity : AppCompatActivity() {
     private fun isOperator(s: String): Boolean{
         return s in listOf("+","-","÷","×","%")
     }
+
 }
 
 class Calculator{
-
 
     fun evaluateExpression(expression:String): Double {
         val postfixExpression = infixToPostfix(expression)
@@ -162,8 +164,9 @@ class Calculator{
         val result = StringBuilder()
         val operatorStack = Stack<Char>()
         for (char in expression) {
-            if (char.isDigit() || char == '.' ) {
+            if (char.isNumeric() || char == '.' ) {
                 result.append(char)
+                result.append(" ")//new change
             } else if (char.isOperator()) {
                 while (operatorStack.isNotEmpty() && char.precedence() <= operatorStack.peek().precedence()){
                     result.append(" ").append(operatorStack.pop()).append(" ")
@@ -181,12 +184,47 @@ class Calculator{
         while (operatorStack.isNotEmpty()){
             result.append(" ").append(operatorStack.pop()).append(" ")
         }
+        println("Result = $result")//to check
         return result.toString().trim()//why is this trim is used
     }
 
+    private fun evaluatePostfixxpression(postfixExpression: String): Double{
+        val stack = Stack<Double>()
+
+        for(char in postfixExpression.split("\\s+".toRegex())){
+            println("splitting the postfixexp at : $char")
+            if(char.isDigitsOnly()){
+                stack.push(char.toDouble())
+
+            }
+            else if(char.isOperator()){
+                val operand2 = stack.pop()
+                val operand1 = stack.pop()
+                println("the popped operands are : $operand1,$operand2")
+                val result = when (char){
+                    "+" -> operand1+operand2
+                    "-" -> operand1-operand2
+                    "%" -> operand1%operand2
+                    "×" -> operand1*operand2
+                    "÷" -> operand1/operand2
+                    else -> throw IllegalArgumentException("Invalid operator : $char")
+                }
+                println("posfix operands result = $result")
+                stack.push(result)
+            }
+        }
+        return if(stack.isEmpty()){
+            throw IllegalArgumentException("Invalid expression")
+        }else{
+            return stack.pop()
+        }
+    }
+
+/*
     private fun Char.isOperator(): Boolean {
         return this in setOf('+', '-', '×', '÷', '%')
     }
+*/
 
     private fun Char.precedence(): Int {
         return when(this){
@@ -195,5 +233,15 @@ class Calculator{
             else -> 0
         }
     }
-
 }
+
+private fun Any.isOperator(): Boolean {
+    return this in setOf('+', '-', '×', '÷', '%')
+}
+
+private fun Any.isNumeric(): Boolean{
+    return this in setOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+}
+
+
+
